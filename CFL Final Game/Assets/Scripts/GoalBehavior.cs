6a2228c93f8928c35eb580d5c1f4bc4b0d61f6d6
rjhutson <72423963+rjhutson@users.noreply.github.com>
2020-11-24 12:56:09 -0500

    creating final game environment

---
+++ CFL Final Game/Assets/Scripts/GoalBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+       //Put collision code here
+       if (collision.gameObject.name == "Marble") {
+         Destroy(this.transform.gameObject);
+
+         Debug.Log("Item collected!");
+       }
+    }
+}
